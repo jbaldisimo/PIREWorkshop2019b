a123a0fb44d9372655092d41f27df6a5d6278641
jbaldisimo <51685929+jbaldisimo@users.noreply.github.com>
2019-06-11 14:05:15 +0800

    Add files via upload
    Final script for biomass in each biogeographic region per trophic level

--- NACRE data set.R
+++ NACRE data set.R
@@ -1,5 +1,7 @@
 #Mean biomass per trophic group for each BGR
-rm(list =ls())
+rm(list=ls())
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
 library(tidyverse)
 library(readxl)
 
@@ -8,7 +10,7 @@ NACRE_Test <- read_excel("C:/Users/JEM BALDISIMO/Desktop/NACRE_Test.xlsx")
 View(NACRE_Test)
 
 #Get only used transect readings for Biomass & show results per BGR & trophic levles
-NACRE_Data <- subset(NACRE_Test, NACRE_used == "yes",
+NACRE_Data <- subset(NACRE_Test, NACRE_used == "yes" & trophicgroup != "no record" & trophicgroup != "#N/A",
                      select=c(BGR, Biomassmtperkm2, trophicgroup))
 NACRE_Data
 
@@ -20,8 +22,14 @@ NACRE_Biomass <- NACRE_Data %>%
   )
 NACRE_Biomass
 
-ggplot(data = NACRE_Biomass) +
-         aes(x = BGR, y = Biomass, fill = as.factor(trophicgroup)) +
+trophiclevel <- ggplot(data = NACRE_Biomass) +
+  aes(x = BGR, y = Biomass, fill = as.factor(trophicgroup)) +
   geom_col() +
-  theme_classic()
+  theme_classic()+
+  theme(axis.text.x = element_text(angle = 90,hjust = 1)) +
+  labs(fill = "Trophic Groups", x = "Biogeographic Region", y = "Total Biomass (mt/km^2)")
 
+trophiclevel 
+png(filename = "trophiclevel.png")
+  trophiclevel 
+dev.off()
